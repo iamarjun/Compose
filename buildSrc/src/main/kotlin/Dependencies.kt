94282e6687d24d14adf2417762af96066607f95b
iamarjun <arjun.manoj@practo.com>
2021-03-05 02:48:30 +0530

    setup buildSrc and define all the dependencies

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,29 @@
+object Dependencies {
+
+    const val kotlin = "1.4.31"
+    const val hilt = "2.33-beta"
+    const val androidGradleWrapper = "7.0.0-alpha08"
+
+    const val compileSdkVersion = 30
+    const val buildToolsVersion = "30.0.3"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 30
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+    const val core = "1.3.2"
+    const val material = "1.3.0"
+    const val compose = "1.0.0-beta01"
+    const val composeActivity = "1.3.0-alpha03"
+    const val composeVideModel = "1.0.0-alpha02"
+
+    const val navigationComponent = "2.3.3"
+
+    const val retrofit = "2.9.0"
+
+    const val paging3 = "3.0.0-beta01"
+
+    const val accompanist = "0.6.1"
+
+    const val hiltViewModel = "1.0.0-alpha03"
+}
