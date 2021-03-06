adb1366ff877202f717a58514594844719518260
iamarjun <arjun.manoj@practo.com>
2021-03-06 15:53:30 +0530

    create base fragment

---
+++ app/src/main/java/com/example/jetpackcompose/ui/BaseFragment.kt
@@ -0,0 +1,27 @@
+package com.example.jetpackcompose.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+
+    @Composable
+    abstract fun Content()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Content()
+            }
+        }
+    }
+}
