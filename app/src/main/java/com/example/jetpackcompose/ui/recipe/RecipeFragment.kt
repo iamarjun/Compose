68eef752fb41c52662d27f357b68023a4bea720b
iamarjun <arjun.manoj@practo.com>
2021-03-06 15:54:29 +0530

    create RecipeFragment.kt and RecipeListFragment.kt

---
+++ app/src/main/java/com/example/jetpackcompose/ui/recipe/RecipeFragment.kt
@@ -0,0 +1,20 @@
+package com.example.jetpackcompose.ui.recipe
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcompose.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RecipeFragment : BaseFragment() {
+
+    @Composable
+    override fun Content() {
+        Column(modifier = Modifier.padding(16.dp)) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jetpackcompose/ui/recipeList/RecipeListFragment.kt
@@ -0,0 +1,14 @@
+package com.example.jetpackcompose.ui.recipeList
+
+import androidx.compose.runtime.Composable
+import com.example.jetpackcompose.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RecipeListFragment : BaseFragment() {
+
+    @Composable
+    override fun Content() {
+        TODO("Not yet implemented")
+    }
+}
