934a8eadc68cf34a2b54b559e0ca7e3e44aee3ef
iamarjun <arjun.manoj@practo.com>
2021-03-12 02:19:31 +0530

    lib upgrades

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,9 +1,5 @@
 object Dependencies {
 
-    const val kotlin = "1.4.30"
-    const val hilt = "2.33-beta"
-    const val androidGradleWrapper = "7.0.0-alpha08"
-
     const val compileSdkVersion = 30
     const val buildToolsVersion = "30.0.3"
     const val minSdkVersion = 21
@@ -11,21 +7,18 @@ object Dependencies {
     const val versionCode = 1
     const val versionName = "1.0"
 
+    const val kotlin = "1.4.30"
+    const val hilt = "2.33-beta"
+    const val androidGradleWrapper = "7.0.0-alpha09"
     const val core = "1.3.2"
     const val material = "1.3.0"
-    const val compose = "1.0.0-beta01"
-    const val composeActivity = "1.3.0-alpha03"
-    const val composeVideModel = "1.0.0-alpha02"
-
-    const val navigationComponent = "2.3.3"
-
+    const val compose = "1.0.0-beta02"
+    const val composeActivity = "1.3.0-alpha04"
+    const val composeVideModel = "1.0.0-alpha03"
+    const val navigationComponent = "2.3.4"
     const val uiTooling = "1.0.0-alpha07"
-
     const val retrofit = "2.9.0"
-
-    const val paging3 = "3.0.0-beta01"
-
-    const val accompanist = "0.6.1"
-
+    const val paging3 = "3.0.0-beta02"
+    const val accompanist = "0.6.2"
     const val hiltViewModel = "1.0.0-alpha03"
 }
