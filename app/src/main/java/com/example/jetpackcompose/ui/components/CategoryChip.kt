0c4f5eca6061469d7161266d1bcbf0964d4d0599
iamarjun <arjun.manoj@practo.com>
2021-03-12 23:18:42 +0530

    create custom category chip

---
+++ app/src/main/java/com/example/jetpackcompose/ui/components/CategoryChip.kt
@@ -0,0 +1,54 @@
+package com.example.jetpackcompose.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcompose.R
+
+@Composable
+fun CategoryChip(
+    category: String,
+    isSelected: Boolean = false,
+    onSelectedCategorySelected: (String) -> Unit,
+) {
+    Surface(
+        modifier = Modifier.padding(4.dp),
+        elevation = 8.dp,
+        shape = RoundedCornerShape(
+            size = 16.dp
+        ),
+        color = when {
+            isSelected -> colorResource(R.color.teal_200)
+            else -> {
+                colorResource(R.color.purple_500)
+            }
+        }
+    ) {
+        Row(modifier = Modifier
+            .toggleable(
+                value = isSelected,
+                onValueChange = {
+                    onSelectedCategorySelected(category)
+                }
+            )) {
+            Text(
+                text = category,
+                style = MaterialTheme.typography.body2,
+                color = Color.White,
+                modifier = Modifier.padding(
+                    vertical = 8.dp,
+                    horizontal = 12.dp
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jetpackcompose/ui/recipeList/FoodCategory.kt
@@ -0,0 +1,24 @@
+package com.example.jetpackcompose.ui.recipeList
+
+import com.example.jetpackcompose.ui.recipeList.FoodCategory.*
+
+enum class FoodCategory(val value: String) {
+    CHICKEN("Chicken"),
+    BEEF("Beef"),
+    SOUP("Soup"),
+    DESSERT("Dessert"),
+    VEGETARIAN("Vegetarian"),
+    MILK("Milk"),
+    VEGAN("Vegan"),
+    PIZZA("Pizza"),
+    DONUT("Donut"),
+}
+
+fun getAllFoodCategories(): List<FoodCategory> {
+    return listOf(CHICKEN, BEEF, SOUP, DESSERT, VEGETARIAN, MILK, VEGAN, PIZZA, DONUT)
+}
+
+fun getFoodCategory(value: String): FoodCategory? {
+    val map = values().associateBy(FoodCategory::value)
+    return map[value]
+}
