fe42b519c9d24868f7672db7f7bf7801fbdb8234
iamarjun <arjunmanoj1995@gmail.com>
2021-04-08 10:40:56 +0530

    refactor to kotlin gradle scripts

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.0-alpha14")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.33-beta")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.4")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
