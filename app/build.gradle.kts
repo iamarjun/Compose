320dbaafd091171c029241c12b08d670694c9497
iamarjun <arjunmanoj1995@gmail.com>
2021-04-21 21:48:42 +0530

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,23 +67,23 @@ dependencies {
     implementation("androidx.compose.material:material:1.0.0-beta04")
     implementation("androidx.compose.material:material-icons-core:1.0.0-beta04")
     implementation("androidx.compose.material:material-icons-extended:1.0.0-beta04")
-    implementation("androidx.activity:activity-compose:1.3.0-alpha05")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha03")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha06")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha04")
     implementation("androidx.compose.runtime:runtime-livedata:1.0.0-beta04")
     implementation("androidx.compose.runtime:runtime-rxjava2:1.0.0-beta04")
 
     implementation("androidx.ui:ui-tooling:1.0.0-alpha07")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
-    implementation("com.google.dagger:hilt-android:2.33-beta")
-    kapt("com.google.dagger:hilt-compiler:2.33-beta")
+    implementation("com.google.dagger:hilt-android:2.34.1-beta")
+    kapt("com.google.dagger:hilt-compiler:2.34.1-beta")
 
     implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
 
-    implementation("com.google.accompanist:accompanist-glide:0.7.0")
+    implementation("com.google.accompanist:accompanist-glide:0.7.1")
 }
